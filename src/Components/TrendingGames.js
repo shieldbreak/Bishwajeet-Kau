4cfb97f3ca3f44c13e486d969e733036ff1717b1
shieldbreak <bishwajeetkaushal@gmail.com>
2023-12-07 14:53:19 +0530

    abc

--- src/Components/TrendingGames.js
+++ src/Components/TrendingGames.js
@@ -8,7 +8,7 @@ function TrendingGames({gameList}) {
     <div className='mt-5 hidden md:block'>
         <h2 className='font-bold text-[30px] dark:text-white'>Trending Games</h2>
     <div className=' md:grid md:grid-cols-3 gap-4 mt-5  lg:grid-cols-4'>
-       {gameList.map((item,index)=>index<4&&(
+       {gameList.map((item,index)=>index<16&&(
         <div className='bg-[#76a8f75e] rounded-lg group
         hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer'>
             <img src={item.background_image} 
